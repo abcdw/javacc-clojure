cb172d68f7abfdc1c15004048f2b72616cf81c98
abcdw <andrewtropin@gmail.com>
2017-04-25 20:48:36 +0300

    Improve sample.clj

--- sample.clj
+++ sample.clj
@@ -3,7 +3,12 @@
 [nil nil]
 #{nil "test"}
 
-[\a \newline \u2354 2/4 35.07e5 :keyw :aoeu/eto]
+[\a \newline \u2354 2/4 35.07e5 :keyw :aoeu/eto
+ #"test-regex"
+ #'var
+ \%
+ #(+ %& 1)
+ ]
 
 (. thess cho-ec?)
 (.ou)
